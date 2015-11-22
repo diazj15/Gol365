4bb07dbc7f2e594523b5a892b61bfde6e24468c3
Junior Diaz <jr_diaz_cr@hotmail.com>
2015-11-22 19:52:44 -0200

    Creating section for Next Games

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,14 +5,14 @@ class PagesController < ApplicationController
 
   	if params[:championship]
 		@news_main = News.where("category = 'Main'").where("championship = ?", params[:championship]).order(updated_at: :desc).last(@NewsLimit)
-		@news_secondary = News.where("category = 'Secondary'").where("championship = ?", params[:championship]).order(updated_at: :desc).last(@NewsLimit)
+		@news_next_games = News.where("category = 'Next Games'").where("championship = ?", params[:championship]).order(updated_at: :desc).last(1)
 		@news_day_like_today = News.where("category = 'A day like today'").where("championship = ?", params[:championship]).order(updated_at: :desc).last(1)
 		@news_know_better_to = News.where("category = 'Know better to'").where("championship = ?", params[:championship]).order(updated_at: :desc).last(1)
 		@news_things_we_learned = News.where("category = '3 things we learned'").where("championship = ?", params[:championship]).order(updated_at: :desc).last(1)
 		@news_player_week = News.where("category = 'Player of the week'").where("championship = ?", params[:championship]).order(updated_at: :desc).last(1)
 	else
 		@news_main = News.where("category = 'Main'").order(updated_at: :desc).last(@NewsLimit)
-		@news_secondary = News.where("category = 'Secondary'").order(updated_at: :desc).last(@NewsLimit)
+		@news_next_games = News.where("category = 'Next Games'").order(updated_at: :desc).last(1)
 		@news_day_like_today = News.where("category = 'A day like today'").order(updated_at: :desc).last(1)
 		@news_know_better_to = News.where("category = 'Know better to'").order(updated_at: :desc).last(1)
 		@news_things_we_learned = News.where("category = '3 things we learned'").order(updated_at: :desc).last(1)
