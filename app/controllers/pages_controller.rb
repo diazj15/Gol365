2c467653bfb62734888480f19227f36839d5c179
Junior Diaz <jr_diaz_cr@hotmail.com>
2015-11-25 01:25:39 -0200

    Removing sections by country and puting all in the same news page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,14 +4,14 @@ class PagesController < ApplicationController
   	@NewsLimit = 20
 
   	if params[:championship]
-		@news_main = News.where("category = 'Main'").where("championship = ?", params[:championship]).order(updated_at: :desc).last(@NewsLimit)
+		@news_main = News.where("category = 'Main'").where("championship = ?", params[:championship]).order(updated_at: :desc).first(@NewsLimit)
 		@news_team_positions = News.where("category = 'Team Positions'").where("championship = ?", params[:championship]).order(updated_at: :desc)
 		@news_next_games = News.where("category = 'Next Games'").where("championship = ?", params[:championship]).order(updated_at: :desc)
 		@news_top_scorers = News.where("category = 'Top Scorers'").where("championship = ?", params[:championship]).order(updated_at: :desc)
-		@news_day_like_today = News.where("category = 'A day like today'").where("championship = ?", params[:championship]).order(updated_at: :desc).last(1)
-		@news_know_better_to = News.where("category = 'Know better to'").where("championship = ?", params[:championship]).order(updated_at: :desc).last(1)
-		@news_things_we_learned = News.where("category = '3 things we learned'").where("championship = ?", params[:championship]).order(updated_at: :desc).last(1)
-		@news_player_week = News.where("category = 'Player of the week'").where("championship = ?", params[:championship]).order(updated_at: :desc).last(1)
+		@news_day_like_today = News.where("category = 'A day like today'").where("championship = ?", params[:championship]).order(updated_at: :desc).first(1)
+		@news_know_better_to = News.where("category = 'Know better to'").where("championship = ?", params[:championship]).order(updated_at: :desc).first(1)
+		@news_things_we_learned = News.where("category = '3 things we learned'").where("championship = ?", params[:championship]).order(updated_at: :desc).first(1)
+		@news_player_week = News.where("category = 'Player of the week'").where("championship = ?", params[:championship]).order(updated_at: :desc).first(1)
 	else
 		@news_main = News.where("category = 'Main'").order(updated_at: :desc).last(@NewsLimit)
 		@news_next_games = News.where("category = 'Next Games'").order(updated_at: :desc)
