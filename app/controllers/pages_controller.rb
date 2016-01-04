090a570bdf3ce7032b8e06c0fa61da1488d63c6f
Junior Diaz <jr_diaz_cr@hotmail.com>
2016-01-04 01:31:17 -0200

    Adding Top Goals section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,7 @@ class PagesController < ApplicationController
 		@news_previous_games = News.where("category = 'Previous Games'").where("championship = ?", params[:championship]).order(updated_at: :desc)
 		@news_next_games = News.where("category = 'Next Games'").where("championship = ?", params[:championship]).order(updated_at: :desc)
 		@news_top_scorers = News.where("category = 'Top Scorers'").where("championship = ?", params[:championship]).order(updated_at: :desc)
+		@news_top_goals = News.where("category = 'Top Goals'").where("championship = ?", params[:championship]).order(updated_at: :desc)
 		@news_day_like_today = News.where("category = 'A day like today'").where("championship = ?", params[:championship]).order(updated_at: :desc).first(1)
 		@news_know_better_to = News.where("category = 'Know better to'").where("championship = ?", params[:championship]).order(updated_at: :desc).first(1)
 		@news_things_we_learned = News.where("category = '3 things we learned'").where("championship = ?", params[:championship]).order(updated_at: :desc).first(1)
@@ -19,6 +20,7 @@ class PagesController < ApplicationController
 		@news_previous_games = News.where("category = 'Previous Games'").order(updated_at: :desc)
 		@news_team_positions = @news_next_games
 		@news_top_scorers = @news_next_games
+		@news_top_goals = News.where("category = 'Top Goals'").order(updated_at: :desc).first(1)
 		@news_day_like_today = News.where("category = 'A day like today'").order(updated_at: :desc).first(1)
 		@news_know_better_to = News.where("category = 'Know better to'").order(updated_at: :desc).first(1)
 		@news_things_we_learned = News.where("category = '3 things we learned'").order(updated_at: :desc).first(1)
